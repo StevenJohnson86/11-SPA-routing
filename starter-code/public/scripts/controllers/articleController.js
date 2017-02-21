91e847e841e081e914734b5d01c934613e137f83
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-20 20:07:13 -0800

    Add TODO after insertRecord was fixed for cleanup

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -4,6 +4,7 @@
   const articleController = {};
   // FIXME: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
+  // TODO: We have multiple instances added to the page. Track down which invokation should be used.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
   articleController.init = function () {
     Article.fetchAll(articleView.initIndexPage);
