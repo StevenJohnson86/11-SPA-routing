850daeda224333d022b40d9c86aaca9c7da5e91b
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-21 09:41:03 -0800

    Adjust jQuery selector when removing section
    Only the direct descendant of the main element should be hidden; this
     ensures the "grandchild" section element is shown again on the About
     page.

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -2,13 +2,12 @@
 
 (function(module) {
   const articleController = {};
-  // FIXME: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
-  // TODO: We have multiple instances added to the page. Track down which invokation should be used.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
   articleController.init = function () {
     Article.fetchAll(articleView.initIndexPage);
-    $('main section').hide();
+    $('main > section').hide();
     $('#articles').show();
   }
   module.articleController = articleController;
